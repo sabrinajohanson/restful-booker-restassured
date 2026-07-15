44d67903229a866e4b97d1e0c93e6b2f32cbf2bc
Sabrina Johanson <67447456+sabrinajohanson@users.noreply.github.com>
2026-07-15 02:25:50 -0300

    feat: attach request/response details to Allure report

--- src/test/java/com/sabrina/api/config/BaseTest.java
+++ src/test/java/com/sabrina/api/config/BaseTest.java
@@ -1,5 +1,6 @@
 package com.sabrina.api.config;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
 
@@ -12,5 +13,8 @@ public class BaseTest {
 
         // Log all requests and responses on failure
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        // Attach full request/response details to the Allure report for every call
+        RestAssured.filters(new AllureRestAssured());
     }
 }
